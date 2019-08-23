d8232dd31ab51d6f24084daa8aad91ed4d594de2
Sigamony <bsigamon@38f9d390472c.ant.amazon.com>
2019-08-23 15:04:27 -0400

    added panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import CoursesService from './courses.service';
 import { FavouriteComponent } from './favourite/favourite.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    FavouriteComponent
+    FavouriteComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-favourite',
   templateUrl: './favourite.component.html',
-  styleUrls: ['./favourite.component.css']
+  styleUrls: ['./favourite.component.css'],
+  encapsulation:ViewEncapsulation.Emulated
 })
 export class FavouriteComponent implements OnInit {
 
   @Input('isFavorite') isFavorite = true;
+  //@Output() change = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -15,5 +18,6 @@ export class FavouriteComponent implements OnInit {
 
   onClick(){
     this.isFavorite = !this.isFavorite;
+    //this.change.emit("true");
   }
 }
