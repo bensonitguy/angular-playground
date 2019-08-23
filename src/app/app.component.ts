af317aa53a3fcba49a5095ac395fc5f50b6caefe
Sigamony <bsigamon@38f9d390472c.ant.amazon.com>
2019-08-23 15:32:28 -0400

    updated message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Benson App';
+  courses = [];
   post = {
     title:"Hello there buddy!!",
     isFavorite: true
